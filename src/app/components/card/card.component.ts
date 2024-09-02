cc08a187eb9cc83df43a96b8e3d6f8333df7a73f
Gabis <gabrielabcarro@gmail.com>
2024-09-01 23:56:35 -0300

    adding new features and working in the api

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
 
+export class CardComponent  implements OnInit {
+
+corpsebride!: Observable<any>;
+beetlejuice!: Observable<any>;
+alice!: Observable<any>;
+charlie!: Observable<any>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const {corpsebride, beetlejuice,aliceinwonderland,chocolatefactory} = environment.endpoints;
+
+    this.corpsebride = this.http.get(corpsebride);
+    this.beetlejuice = this.http.get(beetlejuice);
+    this.alice = this.http.get(aliceinwonderland);
+    this.charlie = this.http.get(chocolatefactory);
+  }
 }
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { CardComponent } from './app/components/card/card.component';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { ApiKeyInterceptor } from './app/api-key.interceptor';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+bootstrapApplication(CardComponent, {
+    providers: [
+      provideHttpClient(),
+      { provide: HTTP_INTERCEPTORS, useClass: ApiKeyInterceptor, multi: true }
+    ]
+  }).catch(err => console.error(err));
