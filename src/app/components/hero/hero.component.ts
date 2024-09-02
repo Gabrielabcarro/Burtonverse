387acd7dc5e57e22cde742c3b3636a5b345b1f75
Gabis <gabrielabcarro@gmail.com>
2024-09-01 21:25:13 -0300

    adding components

---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [BrowserModule],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+}
+
